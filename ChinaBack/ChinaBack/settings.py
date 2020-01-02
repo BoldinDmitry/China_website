44a22b692cecb23f4880a1e13618df710b0d2125
BoldinDmitry <boldin.dmitry@gmail.com>
2020-01-02 15:47:28 +0200

    db was added to volumes

--- ChinaBack/ChinaBack/settings.py
+++ ChinaBack/ChinaBack/settings.py
@@ -128,10 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = "media"
 MEDIA_URL = "/media/"
+MEDIA_ROOT = "media"
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
