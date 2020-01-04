2286d4d7cc051094b433ef728a1ce9e43bf04a3b
BoldinDmitry <boldin.dmitry@gmail.com>
2020-01-04 14:08:52 +0200

    Entrypoint was fixed

--- ChinaBack/ChinaBack/settings.py
+++ ChinaBack/ChinaBack/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "ChinaBack.middleware.AddHeader"
 ]
 
 ROOT_URLCONF = "ChinaBack.urls"
