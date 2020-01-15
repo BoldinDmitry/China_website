2807dcb8978f9f4f64be0a28603fe6d6592e8143
Timophey Podolenchuk <p.timon17@gmail.com>
2020-01-15 21:48:51 +0300

    chore (frontend) add new build 3

---
+++ ChinaBack/frontend/build/precache-manifest.c133f075e305fbe6d2e67888d59f3133 2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9541a1743186873dd1bd577d57d9c927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6dfcc4a86439099fe5d",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a11d6f11faad85580cde",
+    "url": "/static/css/main.b4a638be.chunk.css"
+  },
+  {
+    "revision": "b6dfcc4a86439099fe5d",
+    "url": "/static/js/2.d58778b5.chunk.js"
+  },
+  {
+    "revision": "88c99fe5e08b156c439836ee506baebb",
+    "url": "/static/js/2.d58778b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a11d6f11faad85580cde",
+    "url": "/static/js/main.54a8f3a4.chunk.js"
+  },
+  {
+    "revision": "b403521a6980cfd724bb",
+    "url": "/static/js/runtime-main.97667482.js"
+  }
+]);
