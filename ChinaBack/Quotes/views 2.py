29da79e37b64c2f59b0ae08aa67527b86ac0fd74
Timophey Podolenchuk <p.timon17@gmail.com>
2020-01-15 21:42:49 +0300

    chore (frontend) add new build 3

---
+++ ChinaBack/Quotes/views 2.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import Quote
+from .serializers import QuoteSerializer
+
+
+class RandomQuoteView(viewsets.ReadOnlyModelViewSet):
+    """
+        Lists information related to the current user.
+    """
+
+    serializer_class = QuoteSerializer
+    permission_classes = (AllowAny,)
+
+    def get_object(self):
+        return Quote.objects.order_by("?").first()
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        return Response(serializer.data)
