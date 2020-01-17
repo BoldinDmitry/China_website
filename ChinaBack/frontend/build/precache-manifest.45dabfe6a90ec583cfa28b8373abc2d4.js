8ac41ef79faad322ad2f94c52c14f39ba456436f
Timophey Podolenchuk <p.timon17@gmail.com>
2020-01-18 00:18:01 +0300

    fix spelling

---
+++ ChinaBack/frontend/build/precache-manifest.45dabfe6a90ec583cfa28b8373abc2d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cb3428c2d1fe694c65f1d34b834a80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "3fd9f41a014c9aae6d84",
+    "url": "/static/css/main.f2114b21.chunk.css"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/js/2.eb0c8d57.chunk.js"
+  },
+  {
+    "revision": "88c99fe5e08b156c439836ee506baebb",
+    "url": "/static/js/2.eb0c8d57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fd9f41a014c9aae6d84",
+    "url": "/static/js/main.ad96fc63.chunk.js"
+  },
+  {
+    "revision": "b403521a6980cfd724bb",
+    "url": "/static/js/runtime-main.97667482.js"
+  },
+  {
+    "revision": "3a23fc5e85503a506bf723427658b847",
+    "url": "/static/media/Five_Years_Later_Regular_Demo.3a23fc5e.otf"
+  }
+]);
