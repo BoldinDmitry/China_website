1634490252af9c516c0756014f7b0613c5d812d0
Timophey Podolenchuk <p.timon17@gmail.com>
2020-01-17 16:43:05 +0300

    fix fixed headder heights

---
+++ ChinaBack/frontend/build/precache-manifest.41179109c937210ca1764f4fdeddb112.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7230ed80527a0a327e39158f2befc554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "3bbadae1226a125f774d",
+    "url": "/static/css/main.f2114b21.chunk.css"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/js/2.eb0c8d57.chunk.js"
+  },
+  {
+    "revision": "88c99fe5e08b156c439836ee506baebb",
+    "url": "/static/js/2.eb0c8d57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bbadae1226a125f774d",
+    "url": "/static/js/main.c2e6ed17.chunk.js"
+  },
+  {
+    "revision": "b403521a6980cfd724bb",
+    "url": "/static/js/runtime-main.97667482.js"
+  },
+  {
+    "revision": "3a23fc5e85503a506bf723427658b847",
+    "url": "/static/media/Five_Years_Later_Regular_Demo.3a23fc5e.otf"
+  }
+]);
