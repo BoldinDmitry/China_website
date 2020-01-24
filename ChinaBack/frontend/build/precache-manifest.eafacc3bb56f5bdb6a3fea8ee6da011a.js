a4f1d0ddb47da072e196b0ef9d951d25afeb3bb1
Timophey Podolenchuk <p.timon17@gmail.com>
2020-01-24 23:25:09 +0300

    changed requests to be https

---
+++ ChinaBack/frontend/build/precache-manifest.eafacc3bb56f5bdb6a3fea8ee6da011a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e007730b066c6c50134163a98d87356d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "d3f55314630db6ea54b9",
+    "url": "/static/css/main.85b20749.chunk.css"
+  },
+  {
+    "revision": "7e4bc441d160509cc95a",
+    "url": "/static/js/2.eb0c8d57.chunk.js"
+  },
+  {
+    "revision": "88c99fe5e08b156c439836ee506baebb",
+    "url": "/static/js/2.eb0c8d57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3f55314630db6ea54b9",
+    "url": "/static/js/main.0e6c9247.chunk.js"
+  },
+  {
+    "revision": "b403521a6980cfd724bb",
+    "url": "/static/js/runtime-main.97667482.js"
+  },
+  {
+    "revision": "3a23fc5e85503a506bf723427658b847",
+    "url": "/static/media/Five_Years_Later_Regular_Demo.3a23fc5e.otf"
+  }
+]);
